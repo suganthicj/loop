3a7485fc56c8d9119066a390089ac9310fd0c63f
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-22 17:39:47 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,7 @@
+X11=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+Y11=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+Z11=str(input())
+for i in range(20):
+  if(Z11==X11[i]):
+    print(Y11[i])
+    break
